541adad78ad42015e9e119b1d86c4ff4c916d84b
Jose Moreno <jose.moreno@feverup.com>
2023-08-27 11:07:57 +0200

    playing with autocomplete

--- src/main/java/org/fever/YamlCompletionContributor.java
+++ src/main/java/org/fever/YamlCompletionContributor.java
@@ -8,13 +8,38 @@ import com.intellij.codeInsight.lookup.LookupElementBuilder;
 import com.intellij.notification.Notification;
 import com.intellij.notification.NotificationType;
 import com.intellij.notification.Notifications;
+import com.intellij.openapi.fileTypes.FileType;
+import com.intellij.openapi.fileTypes.FileTypeManager;
+import com.intellij.openapi.project.Project;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.PsiManager;
+import com.intellij.psi.util.PsiTreeUtil;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.*;
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
 import java.util.List;
+
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.PsiManager;
+import com.intellij.psi.search.FileTypeIndex;
+import com.intellij.psi.search.GlobalSearchScope;
+//import com.jetbrains.python.PythonFileType;
+
+
+//Project project = ... // Obtain the project instance
+//
+//// Define a scope for the project files
+//        GlobalSearchScope scope = GlobalSearchScope.projectScope(project);
+//
+//// Get all Python files using FileTypeIndex
+//        PsiFile[] pythonFiles = FileTypeIndex.getFiles(PythonFileType.INSTANCE, scope);
 
 
 public class YamlCompletionContributor extends CompletionContributor {
@@ -23,8 +48,64 @@ public class YamlCompletionContributor extends CompletionContributor {
     public YamlCompletionContributor() {
     }
 
+
+    private List<String> findYamlFilesInDependencyInjectionDirs(Path rootPath) throws IOException {
+        List<String> yamlFiles = new ArrayList<>();
+
+        Files.walkFileTree(rootPath, new SimpleFileVisitor<Path>() {
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                if (isYamlFile(file) && isInDependencyInjectionSubDirectory(file)) {
+                    yamlFiles.add(file.toString());
+                }
+                return FileVisitResult.CONTINUE;
+            }
+        });
+
+        return yamlFiles;
+    }
+
+    private boolean isYamlFile(Path path) {
+        String fileName = path.getFileName().toString();
+        return fileName.endsWith(".yaml") || fileName.endsWith(".yml");
+    }
+
+    private boolean isInDependencyInjectionSubDirectory(Path path) {
+
+        if (path.getParent() == null) {
+            return false;
+        }
+        String parentDirectoryName = path.getParent().getFileName().toString();
+
+        while (!parentDirectoryName.equals("_dependency_injection")) {
+            if (path.getParent() == null) {
+                return false;
+            }
+            path = path.getParent();
+            parentDirectoryName = path.getParent().getFileName().toString();
+        }
+
+        return true;
+    }
     public void beforeCompletion(@NotNull CompletionInitializationContext context) {
-        String currentProjectBasePath = context.getProject().getBasePath();
+        Project project = context.getProject();
+        FileTypeManager fileTypeManager = FileTypeManager.getInstance();
+        FileType fileType = fileTypeManager.getFileTypeByFileName("hola.py");
+
+
+//        // list of python files in the project
+//        files = DirectoryUtil.findFilesByMask(
+//                project.getBaseDir(),
+//                "*.py",
+//                -1
+//        );
+
+        // get the current file as a virtualFile
+//        PsiFile currentFile = context.getFile();
+//        PyClassType[] classes = PsiTreeUtil.getChildrenOfType(psiFile, PyClass.class);
+
+//        PsiFile psiFile = PsiManager.getInstance(project).findFile(currentFile.getVirtualFile());
+        String currentProjectBasePath = project.getBasePath();
         if (currentProjectBasePath == null) {
             Notifications.Bus.notify(new Notification(
                     "pypendency",
@@ -35,38 +116,80 @@ public class YamlCompletionContributor extends CompletionContributor {
             return;
         }
 
-        this.completions = this.generateCompletion(currentProjectBasePath);
-    }
 
-    private List<String> generateCompletion(String currentProjectBasePath) {
-        List<String> output = new ArrayList<>();
-        try {
-            String[] cmd = new String[]{
-                    "/bin/sh", "-c", "~/.pypendency/list_container_keys.py " + currentProjectBasePath
-            };
-            Process process = Runtime.getRuntime().exec(cmd);
+//            Notifications.Bus.notify(new Notification(
+//                    "pypendency",
+//                    "Pypendency",
+//                    context.getEditor().getDocument().getText(),
+//                    NotificationType.WARNING
+//            ));
+//            return;
 
-            if (notifyErrors(process)) return output;
 
-            String s;
-            BufferedReader stdInput = new BufferedReader(
-                    new InputStreamReader(process.getInputStream())
-            );
-            while ((s = stdInput.readLine()) != null) {
-                output.add(s);
-            }
+        // populate completions with string hello if the character before the caret is a double quote
+        int startOffset = context.getStartOffset();
 
-            process.waitFor();
-        } catch (Exception e) {
-            Notifications.Bus.notify(new Notification(
-                    "pypendency",
-                    "Pypendency",
-                    e.toString(),
-                    NotificationType.WARNING
-            ));
+        if (startOffset < 2) {
+            this.completions = new ArrayList<>();
+            return;
         }
 
-        return output;
+        if (context.getEditor().getDocument().getText().charAt(startOffset - 2) == '"') {
+            this.completions = this.generateCompletion(currentProjectBasePath);
+        }
+
+
+    }
+
+    // write a function to generate the completions for all python fqns in the project
+    // this function should be called in the beforeCompletion method
+
+
+
+
+    private List<String> generateCompletion(String currentProjectBasePath) {
+        try {
+//            List<String> yamlFiles = findYamlFilesInDependencyInjectionDirs(Paths.get(Objects.requireNonNull(project.getBasePath())+"/src"));
+            List<String> yamlFiles = findYamlFilesInDependencyInjectionDirs(Paths.get(currentProjectBasePath+"/src"));
+            return yamlFiles;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+//        List<String> output = new ArrayList<>(
+//            List.of(
+//                        "foo",
+//                        "bar",
+//                        "baz"
+//                )
+//        );
+
+//        try {
+//            String[] cmd = new String[]{
+//                    "/bin/sh", "-c", "~/.pypendency/list_container_keys.py " + currentProjectBasePath
+//            };
+//            Process process = Runtime.getRuntime().exec(cmd);
+//
+//            if (notifyErrors(process)) return output;
+//
+//            String s;
+//            BufferedReader stdInput = new BufferedReader(
+//                    new InputStreamReader(process.getInputStream())
+//            );
+//            while ((s = stdInput.readLine()) != null) {
+//                output.add(s);
+//            }
+//
+//            process.waitFor();
+//        } catch (Exception e) {
+//            Notifications.Bus.notify(new Notification(
+//                    "pypendency",
+//                    "Pypendency",
+//                    e.toString(),
+//                    NotificationType.WARNING
+//            ));
+//        }
+
+//        return output;
     }
 
     private boolean notifyErrors(Process process) throws IOException {
