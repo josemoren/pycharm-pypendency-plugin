d691bf1230141dfa8bddde518b0e54f90dd421cc
Andrés Durán <andres.duran@feverup.com>
2024-01-05 14:16:57 +0100

    follow references by manually set FQN as final callback

---
+++ src/main/java/org/fever/DependencyInjectionSearchScope.java
@@ -0,0 +1,33 @@
+package org.fever;
+
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.search.GlobalSearchScope;
+import org.jetbrains.annotations.NotNull;
+
+public class DependencyInjectionSearchScope extends GlobalSearchScope {
+
+    public DependencyInjectionSearchScope(@NotNull Project project) {
+        super(project);
+    }
+
+    @Override
+    public boolean contains(@NotNull VirtualFile file) {
+        return file.getPath().contains("/_dependency_injection") && file.getName().endsWith(".py");
+    }
+
+    @Override
+    public int compare(@NotNull VirtualFile file1, @NotNull VirtualFile file2) {
+        return 0;
+    }
+
+    @Override
+    public boolean isSearchInModuleContent(@NotNull com.intellij.openapi.module.Module aModule) {
+        return true;
+    }
+
+    @Override
+    public boolean isSearchInLibraries() {
+        return false;
+    }
+}
--- src/main/java/org/fever/PsiReference.java
+++ src/main/java/org/fever/PsiReference.java
@@ -1,15 +1,23 @@
 package org.fever;
 
 import com.intellij.openapi.util.TextRange;
+import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.*;
+import com.intellij.psi.search.*;
+import com.intellij.util.indexing.FileBasedIndex;
+import com.jetbrains.python.PythonFileType;
 import org.fever.utils.SourceCodeFileResolver;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class PsiReference extends PsiReferenceBase<PsiElement> {
     private final String identifier;
+    private static final String MANUALLY_SET_FQN_GROUP_SELECTOR_REGEX = "container_builder\\.set\\(\\s*\"(\\S+)\"";
 
     public PsiReference(@NotNull PsiElement element, TextRange textRange, String identifier) {
         super(element, textRange);
@@ -26,6 +34,9 @@ public class PsiReference extends PsiReferenceBase<PsiElement> {
         PsiManager psiManager = getElement().getManager();
         PsiElement file = resolveToDependencyInjectionFileFromIdentifier(identifier, psiManager);
 
+        if (file == null) {
+            file = resolveToDependencyInjectionManualDeclaration(identifier, psiManager);
+        }
         if (file == null) {
             file = tryFallingBackToSourceCodeFileUsingIdentifierAsFqn(identifier, psiManager);
         }
@@ -71,6 +82,26 @@ public class PsiReference extends PsiReferenceBase<PsiElement> {
         String relativeFilePath = String.join("/", Arrays.copyOfRange(parts, 1, parts.length - 1));
 
         return absoluteBasePath + "/src/" + djangoAppName + GotoPypendencyOrCodeHandler.DEPENDENCY_INJECTION_FOLDER + relativeFilePath;
+    }
+
+    private PsiElement resolveToDependencyInjectionManualDeclaration(String identifier, PsiManager psiManager) {
+        GlobalSearchScope scope = new DependencyInjectionSearchScope(getElement().getProject());
+        Collection<VirtualFile> dependencyInjectionFiles = FileBasedIndex.getInstance()
+                .getContainingFiles(
+                        FileTypeIndex.NAME,
+                        PythonFileType.INSTANCE,
+                        scope);
+
+        for (VirtualFile file : dependencyInjectionFiles) {
+            PsiFile psiFile = psiManager.findFile(file);
+            assert psiFile != null;
+            String fileContent = psiFile.getText();
+            Matcher matcher = Pattern.compile(MANUALLY_SET_FQN_GROUP_SELECTOR_REGEX).matcher(fileContent);
+            if (matcher.find() && matcher.group(1).equals(identifier)) {
+                return psiFile;
+            }
+        }
+        return null;
     }
 
     public static @Nullable PsiFile tryFallingBackToSourceCodeFileUsingIdentifierAsFqn(String identifier, PsiManager psiManager) {
