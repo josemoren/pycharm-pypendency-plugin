8e86fcad2dee0a32612fa80ed108410a4f9009d1
Mario Moreno Zarco <mario.moreno@feverup.com>
2024-01-12 12:08:34 +0100

    * Add the comment in the docstring.

--- src/main/java/org/fever/PsiReference.java
+++ src/main/java/org/fever/PsiReference.java
@@ -25,32 +25,13 @@ public class PsiReference extends PsiReferenceBase<PsiElement> {
     @Override
     public @Nullable PsiElement resolve() {
         PsiManager psiManager = getElement().getManager();
-        PsiElement file = null;
+        PsiElement file = resolveToFqnsDependencyInjectionFile(fqn, psiManager);
 
-        if (this.fqnMatchesFileName(fqn)) {
-            file = resolveSourceCodeFileFromCurrentDependencyInjectionFile(psiManager);
-        }
-        if (file == null) {
-            file = resolveToFqnsDependencyInjectionFile(fqn, psiManager);
-        }
         if (file == null) {
             file = SourceCodeFileResolver.fromFqn(fqn, psiManager);
         }
 
         return file;
-    }
-
-    private boolean fqnMatchesFileName(String fqn) {
-        String fqnClassName = fqn.substring(fqn.lastIndexOf(".") + 1);
-        String equivalentClassName = CaseFormatter.camelCaseToSnakeCase(fqnClassName) + ".py";
-        String fileName = getElement().getContainingFile().getName();
-        return equivalentClassName.equals(fileName);
-    }
-
-    private @Nullable PsiElement resolveSourceCodeFileFromCurrentDependencyInjectionFile(PsiManager psiManager) {
-        String dependencyInjectionFilePath = getElement().getContainingFile().getVirtualFile().getCanonicalPath();
-        assert dependencyInjectionFilePath != null;
-        return SourceCodeFileResolver.fromDependencyInjectionFilePath(dependencyInjectionFilePath, psiManager);
     }
 
     private @Nullable PsiElement resolveToFqnsDependencyInjectionFile(String fqn, PsiManager psiManager) {
@@ -74,10 +55,15 @@ public class PsiReference extends PsiReferenceBase<PsiElement> {
         return null;
     }
 
+    /**
+     * Gets the absolute file path of the DI file without the file extension.
+     * For example:
+     *     fqn: core.infrastructure.user.finders.core_user_finder.CoreUserFinder
+     *     return: <PROJECT_PATH>/src/core/_dependency_injection/infrastructure/user/finders/core_user_finder
+     * @param fqn full qualified name of the class.
+     * @return string with the full path of the DI file without the file extension.
+     */
     private String getAbsoluteDependencyInjectionFilePathWithoutExtension(String fqn) {
-        // Input: core.infrastructure.user.finders.core_user_finder.CoreUserFinder
-        // Output: core/_dependency_injection/infrastructure/user/finders/core_user_finder
-
         String absoluteBasePath = getElement().getProject().getBasePath();
         String[] parts = fqn.split("\\.");
         String djangoAppName = parts[0];
