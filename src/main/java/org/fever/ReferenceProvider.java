55dfb9834c0ce03041140ce5043dd48fe4e84f44
Mario Moreno Zarco <mario.moreno@feverup.com>
2024-01-12 12:48:54 +0100

    * Fix some name inconsistencies (fqn!=identifier)

--- src/main/java/org/fever/ReferenceProvider.java
+++ src/main/java/org/fever/ReferenceProvider.java
@@ -4,32 +4,33 @@ import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiReferenceProvider;
 import com.intellij.util.ProcessingContext;
-import org.fever.utils.FqnExtractor;
 import org.jetbrains.annotations.NotNull;
 
 public class ReferenceProvider extends PsiReferenceProvider {
+    public static final String IDENTIFIER_REGEX = "^\"?@?[a-z0-9_.]+\\.[A-Za-z0-9_]+\"?$";
+
     @Override
     public com.intellij.psi.PsiReference @NotNull [] getReferencesByElement(@NotNull PsiElement element, @NotNull ProcessingContext context) {
         String text = element.getText();
-        if (!text.matches(FqnExtractor.FQN_REGEX)) {
+        if (!text.matches(IDENTIFIER_REGEX)) {
             return org.fever.PsiReference.EMPTY_ARRAY;
         }
 
-        org.fever.PsiReference reference = getReferenceForFqn(element, text);
+        org.fever.PsiReference reference = getReferenceForIdentifier(element, text);
 
         return new org.fever.PsiReference[]{reference};
     }
 
     @NotNull
-    private static org.fever.PsiReference getReferenceForFqn(@NotNull PsiElement element, String fqn) {
+    private static org.fever.PsiReference getReferenceForIdentifier(@NotNull PsiElement element, String identifier) {
         TextRange range;
-        if (hasQuotes(fqn)) {
-             range = new TextRange(1, fqn.length() - 1);
+        if (hasQuotes(identifier)) {
+             range = new TextRange(1, identifier.length() - 1);
         } else {
-            range = new TextRange(0, fqn.length());
+            range = new TextRange(0, identifier.length());
         }
 
-        return new org.fever.PsiReference(element, range, fqn);
+        return new org.fever.PsiReference(element, range, identifier);
     }
 
     private static boolean hasQuotes(String fqn) {
