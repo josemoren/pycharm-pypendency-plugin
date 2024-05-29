cfed36fd3fe99be4e9a3a37334d614b457a7d350
Andrés Durán <andres.duran@feverup.com>
2024-05-29 14:12:33 +0200

    Go to injected implementation

---
+++ src/main/java/org/fever/utils/RegexMatcher.java
@@ -0,0 +1,14 @@
+package org.fever.utils;
+
+import java.util.regex.MatchResult;
+import java.util.regex.Pattern;
+
+public class RegexMatcher {
+    public static String[] getAllMatches(String text, String regex) {
+        return Pattern.compile(regex)
+                .matcher(text)
+                .results()
+                .map(MatchResult::group)
+                .toArray(String[]::new);
+    }
+}
