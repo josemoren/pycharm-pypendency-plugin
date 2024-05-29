edbffe39cc43a8f0b898918429b0a932720507d2
Andrés Durán <andres.duran@feverup.com>
2024-05-29 13:38:38 +0200

    Add identifier autocompletions when typing based on the cache content

--- src/main/java/org/fever/ResolutionCache.java
+++ src/main/java/org/fever/ResolutionCache.java
@@ -1,18 +1,22 @@
 package org.fever;
 
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.components.*;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 @State(
         name = "ReferenceCache",
         storages = {@Storage(StoragePathMacros.CACHE_FILE)}
 )
-final class ResolutionCache implements PersistentStateComponent<ResolutionCache.State> {
-    static class State {
+public final class ResolutionCache implements PersistentStateComponent<ResolutionCache.State> {
+    public static class State {
         public Map<String, Map<String, String>> resolutionCache;
 
         public State() {
@@ -34,6 +38,23 @@ final class ResolutionCache implements PersistentStateComponent<ResolutionCache.
         public void removeCachedResolution(String projectName, String identifier) {
             this.getResolutionCacheForProject(projectName).remove(identifier);
         }
+
+        // This returns a Collection<String> because there may be multiple identifiers for the same className in the same project.
+        public Collection<String> getCachedIdentifiersByClassName(String projectName, String className) {
+            return this.getResolutionCacheForProject(projectName).keySet().stream()
+                    .filter(identifier -> identifier != null && identifier.endsWith("." + className))
+                    .collect(Collectors.toList());
+        }
+
+        public int countIdentifiers(String projectName) {
+            return this.getResolutionCacheForProject(projectName).keySet().size();
+        }
+
+        public List<String> fuzzyFindIdentifiersMatching(String projectName, String identifier) {
+            return this.getResolutionCacheForProject(projectName).keySet().stream()
+                    .filter(key -> key.toLowerCase().contains(identifier.toLowerCase()))
+                    .toList();
+        }
     }
 
     private State myState = new State();
@@ -44,5 +65,9 @@ final class ResolutionCache implements PersistentStateComponent<ResolutionCache.
 
     public void loadState(@NotNull State state) {
         myState = state;
+    }
+
+    public static State getInstance() {
+        return ApplicationManager.getApplication().getService(ResolutionCache.class).getState();
     }
 }
