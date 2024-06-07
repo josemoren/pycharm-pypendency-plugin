829fabf08ae9691cc4971aebfc3c1222ab2a6684
Mario Moreno Zarco <mario.moreno@feverup.com>
2024-06-07 13:18:54 +0200

    * Small refactor

--- src/main/java/org/fever/filecreator/ClassArgumentFetcher.java
+++ src/main/java/org/fever/filecreator/ClassArgumentFetcher.java
@@ -8,7 +8,6 @@ import com.jetbrains.python.psi.types.PyClassType;
 import com.jetbrains.python.psi.types.PyType;
 import com.jetbrains.python.psi.types.TypeEvalContext;
 import groovyjarjarantlr4.v4.misc.OrderedHashMap;
-import org.apache.commons.lang.ArrayUtils;
 import org.fever.fileresolver.DependencyInjectionFileResolverByClassName;
 import org.fever.utils.ClassArgumentParser;
 import org.fever.utils.IdentifierExtractor;
@@ -20,6 +19,7 @@ import java.util.stream.Collectors;
 
 public class ClassArgumentFetcher {
     private static final int SELF_INDEX_IN_PARAMETER_LIST = 0;
+
     public static Collection<IdentifierItem> getFqnOfInitArguments(PyClass originClass) {
         List<IdentifierItem> identifiers = new ArrayList<>();
         PyFile sourceCodeFile = (PyFile) originClass.getContainingFile();
@@ -34,16 +34,12 @@ public class ClassArgumentFetcher {
         for (Map.Entry<PyParameter, @Nullable PyClass> entry : initParamsToClasses.entrySet()) {
             PyParameter parameter = entry.getKey();
             PyClass parameterClass = entry.getValue();
-            String className = ClassArgumentParser.parse(parameter.getText());
-            List<String> implementationsClassNames = getAllImplementationsClassNames(parameterClass, className);
-            implementationsClassNames.add(className);
 
-            for (String implementationClassName : implementationsClassNames) {
-                PsiFile dependencyInjectionFile = DependencyInjectionFileResolverByClassName.resolve(project, implementationClassName);
-                String implementationIdentifier = IdentifierExtractor.extractIdentifierFromDIFile(dependencyInjectionFile);
-                IdentifierItem item = new IdentifierItem(implementationIdentifier, parameterClass, parameter);
-                if (!identifiers.contains(item)) {
-                    identifiers.add(item);
+            List<String> classNames = getInjectedClassNameAndImplementations(parameter, parameterClass);
+            for (String className : classNames) {
+                IdentifierItem identifier = getIdentifierItem(className, project, parameter, parameterClass);
+                if (!identifiers.contains(identifier)) {
+                    identifiers.add(identifier);
                 }
             }
         }
@@ -52,25 +48,36 @@ public class ClassArgumentFetcher {
     }
 
     @NotNull
-    private static List<String> getAllImplementationsClassNames(@Nullable PyClass parameterClass, String className) {
-        List<String> EMPTY_LIST = new ArrayList<>();
+    private static List<String> getInjectedClassNameAndImplementations(PyParameter parameter, @Nullable PyClass parameterClass) {
+        String injectedClassName = ClassArgumentParser.parse(parameter.getText());
 
+        List<String> allClassNames = new ArrayList<>();
+        allClassNames.add(injectedClassName);
 
         if (parameterClass == null) {
-            return EMPTY_LIST;
+            return allClassNames;
         }
 
-        boolean classIsBuiltin = Character.isLowerCase(className.charAt(0));
+        boolean classIsBuiltin = Character.isLowerCase(injectedClassName.charAt(0));
         if (classIsBuiltin) {
-            return EMPTY_LIST;
+            return allClassNames;
         }
 
-        return PyClassInheritorsSearch
+        List<String> implementationClassNames = PyClassInheritorsSearch
                 .search(parameterClass, false)
                 .findAll()
                 .stream()
                 .map(PyClass::getName)
                 .collect(Collectors.toList());
+
+        allClassNames.addAll(implementationClassNames);
+        return allClassNames;
+    }
+
+    private static @NotNull IdentifierItem getIdentifierItem(String className, Project project, PyParameter parameter, PyClass parameterClass) {
+        PsiFile dependencyInjectionFile = DependencyInjectionFileResolverByClassName.resolve(project, className);
+        String implementationIdentifier = IdentifierExtractor.extractIdentifierFromDIFile(dependencyInjectionFile);
+        return new IdentifierItem(implementationIdentifier, parameterClass, parameter);
     }
 
     private static Map<PyParameter, @Nullable PyClass> getClassesFromInitParams(PyClass originClass, TypeEvalContext context) {
@@ -78,16 +85,19 @@ public class ClassArgumentFetcher {
         if (initMethod == null) {
             return new HashMap<>();
         }
-        PyParameter[] initParameters = initMethod.getParameterList().getParameters();
-        PyParameter[] initParametersWithoutSelf = (PyParameter[]) ArrayUtils.remove(initParameters, SELF_INDEX_IN_PARAMETER_LIST);
 
+        List<PyParameter> dunderInitMethodParameters = Arrays.asList(initMethod.getParameterList().getParameters());
+        dunderInitMethodParameters.remove(SELF_INDEX_IN_PARAMETER_LIST);
+        
         Map<PyParameter, @Nullable PyClass> paramsToClasses = new OrderedHashMap<>();
-        for (PyParameter parameter : initParametersWithoutSelf) {
+        for (PyParameter parameter : dunderInitMethodParameters) {
             PyType argumentType = parameter.getAsNamed().getArgumentType(context);
+
             if (!(argumentType instanceof PyClassType)) {
                 paramsToClasses.put(parameter, null);
                 continue;
             }
+
             PyClass pyClass = ((PyClassType) argumentType).getPyClass();
             paramsToClasses.put(parameter, pyClass);
         }
