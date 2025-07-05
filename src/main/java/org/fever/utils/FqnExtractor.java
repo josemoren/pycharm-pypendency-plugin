0715bed0de48477d224ea9b7e61b354a64c0b6e4
scastd <scastd00@gmail.com>
2025-07-06 01:40:36 +0200

    Add line marker provider (with icon) for Python DI files

--- src/main/java/org/fever/utils/FqnExtractor.java
+++ src/main/java/org/fever/utils/FqnExtractor.java
@@ -8,10 +8,10 @@ import java.util.regex.Pattern;
 
 public class FqnExtractor {
     private static final String YAML_FQN_GROUP_SELECTOR_REGEX = "fqn:\\s*(\\S+)";
-    private static final String PYTHON_FQN_GROUP_SELECTOR_REGEX = "container_builder\\.set_definition\\(\\s+Definition\\(\\s*\"\\S+\",\\s*\"(\\S+)\"";
+    private static final String PYTHON_FQN_GROUP_SELECTOR_REGEX = "container_builder\\.set_definition\\(\\s*Definition\\(\\s*\"[^\"]+\",\\s*\"([^\"]+)\"";
 
     private static @Nullable String extractGroupFromRegex(String text, String regex) {
-        Matcher matcher = Pattern.compile(regex).matcher(text);
+        Matcher matcher = Pattern.compile(regex, Pattern.DOTALL).matcher(text);
         return matcher.find() ? matcher.group(1) : null;
     }
 
@@ -44,7 +44,6 @@ public class FqnExtractor {
     private static Boolean isPythonFile(String extension) {
         return extension.equals("py");
     }
-
 
     public static @Nullable String extractFqnFromYaml(String fileContent) {
         return extractGroupFromRegex(fileContent, YAML_FQN_GROUP_SELECTOR_REGEX);
